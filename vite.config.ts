f03a3e9062b8372dcacf813467274c68a22d415b
changshuai <changshuai@youint.com>
2022-09-15 18:16:43 +0800

    feat: test pre-commit

---
+++ vite.config.ts
@@ -0,0 +1,53 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+import Unocss from "./config/unocss";
+
+// https://vitejs.dev/config/
+
+const rollupOptions = {
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    // 添加JSX插件
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
+    // 添加UnoCSS插件
+    Unocss(),
+  ],
+
+  build: {
+    rollupOptions,
+    minify: false,
+    cssCodeSplit: true,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
+      // 导出模块格式
+      formats: ["es", "umd", "iife"],
+    },
+  },
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
+  }
+});
