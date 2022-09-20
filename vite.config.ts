7e34083f1810d495bf7ba2ac91bd5bf06aebf702
changshuai <changshuai@youint.com>
2022-09-20 14:38:27 +0800

    feat: 添加工作流

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,8 @@ export default defineConfig({
 
   build: {
     rollupOptions,
-    minify: false,
+    minify: "terser",
+    sourcemap: true,
     cssCodeSplit: true,
     lib: {
       entry: "./src/entry.ts",
@@ -44,10 +45,10 @@ export default defineConfig({
     globals: true,
     // simulate DOM with happy-dom
     // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
+    environment: "happy-dom",
     // 支持tsx组件，很关键
     transformMode: {
-      web: [/.[tj]sx$/]
-    }
-  }
+      web: [/.[tj]sx$/],
+    },
+  },
 });
